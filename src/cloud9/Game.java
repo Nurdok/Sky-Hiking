2192ed87c988832974ba4e6ec227e1329be284a6
nurdok <nurdok@gmail.com>
2011-12-17 15:55:06 +0200

    don't remember.

--- src/cloud9/Game.java
+++ src/cloud9/Game.java
@@ -36,15 +36,34 @@ public class Game {
 			final List<Player> remainingPlayers = new LinkedList<Player>(players);
 			for (final CloudLevel level : CloudLevel.gameLevels()) {
 				if (remainingPlayers.isEmpty()) {
-					//TODO: move to next level
+					break; //Move to next level
 				}
 				Player pilot = pilotIterator.next();
-				while (remainingPlayers.contains(pilot)) {
+				while (!remainingPlayers.contains(pilot)) {
 					pilot = pilotIterator.next();
 				}
 				
 				final Collection<Symbol> diceRoll = Dice.roll(level.getDiceNumber());
+				//TODO: allow each player to decide...
 				
+				for (final Player player : remainingPlayers) { //TODO: order important
+					if (player != pilot) {
+						if (!player.stay()) {
+							scoreboard.addPoints(player, level.getScore());
+							remainingPlayers.remove(player);
+						}
+					}
+				}
+				
+				if (pilot.hasCards(diceRoll)) {
+					pilot.pay(diceRoll, deck);
+				}
+				else {
+					//We crash and move to next round
+					for (final Player player : players) {
+						player.addCards(deck.draw(1));
+					}
+				}
 			}
 		}
 	}
--- src/cloud9/Player.java
+++ src/cloud9/Player.java
@@ -6,5 +6,8 @@ public interface Player {
 	public String getName();
 	public void play(GameState state);
 	public void addCards(Collection<Card> cards);
-	
+	public boolean hasCards(Collection<Symbol> diceRoll);
+	public void pay(Collection<Symbol> diceRoll, Deck deck);
+	public void score(int points);
+	public int getScore();
 }
